fc47677e07d51411ecc63d7e15c38dafc6bf9399
Numair <numair.baseer@gmail.com>
2017-06-01 23:04:02 +0000

    successfully deleted headers

--- www/sites/all/modules/custom/statsform/templates/page--hijacked.tpl.php
+++ www/sites/all/modules/custom/statsform/templates/page--hijacked.tpl.php
@@ -1,4 +1,3 @@
 <h1>This page is hijacked</h1>
 <div>Notice that the regions of the body are gone and replaced by this template.h You will have to learn how to use hook_html_head_alter() in order to modify things in the head element, like jquery version loaded, etc.</div>
 <div><?php print render($page['content']); ?></div> 
-<div><?php dsm($page); ?></div>
